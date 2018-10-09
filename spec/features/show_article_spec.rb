07480c92d7f333ce11327a4679ac640779ee611e
asl331 <ing.asl331@gmail.com>
2018-10-09 14:51:00 -0500

    handle articles not found

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature "Showing an article" do
   scenario "A users shows an article" do
     visit "/"
     click_link @article.title
+    
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
